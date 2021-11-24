5d785996192e675aa29ab6547cc93f1ce96e9ab7
Benish-31 <benishimwe31@gmail.com>
2021-11-24 17:34:34 +0100

    Add project files.

---
+++ Hospital/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee newemp = new Employee();
+
+            
+            string input = null;
+            while(input != "3")
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("To add a student enter 1");
+                Console.WriteLine("To add a teacher enter 2");
+                Console.WriteLine("To quit the program enter 3");
+                Console.WriteLine("Please enter selection: ");
+
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Adding new doctor.");
+
+                            Console.WriteLine("Enter new doctor name: ");
+                            string names = Console.ReadLine();
+                            Scheduled.AddDoctor(new Doctor() { NAME = names });
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Adding new teacher.");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Closing program.");
+                            Console.ReadKey();
+                            break;
+                            }
+                            default:
+{
+                                Console.WriteLine("The input entered was not valid.");
+                                //printMenu();
+                                break;
+                            }
+                        }
+            /*Employee credential = new Employee();
+            Doctor doccy = new Doctor();
+            Nurse nurssy = new Nurse();
+            Administrator admin = new Administrator();
+            Console.WriteLine("Type a letter of your profession: \n [D]octor \n [N]urse \n [A]dministrator");
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D:
+                    credential.EnterCredintials();
+                    doccy.Display();
+                    nurssy.Display();
+                    break;
+                case ConsoleKey.N:
+                    credential.EnterCredintials();
+                    doccy.Display();
+                    nurssy.Display();
+                    break;
+                case ConsoleKey.A:
+                    credential.EnterCredintials();
+                    Console.WriteLine("[S]how list of Doctors, Nurses and Administrators or [M]odify the list");
+                    string enter = Console.ReadLine();
+                    if (enter == "S")
+                    {
+                        doccy.Display();
+                        nurssy.Display();
+                        admin.Display();
+                    }
+                    else if (enter == "M")
+                    {
+                        Console.WriteLine("[D]octor \n [N]urse \n [A]dministrator");
+                        switch (Console.ReadKey(true).Key)
+                        {
+                            case ConsoleKey.D:
+
+                                doccy.Insert();
+                                break;
+                        }
+
+
+
+
+                    }
+                    break;*/
+
+
+            
+
+            
+            
+        }
+    }
+}
---
+++ Hospital/Scheduled.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Scheduled : Employee
+    {
+        public Scheduled(string name, string surname, int PESEL, string username, string password) : base(string name, string surname, int PESEL, string username, string password) { }
+        
+        List<Doctor> Doctors { get; set; }
+        List<Nurse> Nurses { get; set; }
+        
+        public bool AddDoctor (Doctor doctor)
+        {
+            try
+            {
+                this.Doctors.Add(Doctor);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool AddNurse(Nurse nurse)
+        {
+            try
+            {
+                Nurse.Add(nurse);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+       
+
+        /*override public void Display()
+        {
+            base.Display();
+            Console.WriteLine(name + job + speciality + dutySchedule);
+        }
+
+        List<Employee> schedule;
+        public show()
+        {
+            schedule = new List<Employee>();
+        }
+        public void Add*/
+
+    }
+    class Doctor : Scheduled
+    {
+        Nurse nully = new Nurse();
+        private int PWZ;
+        // private string[] speciality = new string[] { "Cardiologist", "Urologist", "Neurologist", "Laryngologist" };
+        public Doctor(int PWZ, string name, string surname, int PESEL, string username, string password) : base(string name, string surname, int PESEL, string username, string password)
+        {
+            this.PWZ = PWZ;
+           
+
+        }
+        public int pwz { get { return PWZ; } }
+        /*public string[] SPECIALITY
+        {
+            get
+            {
+                return speciality;
+            }
+            set
+            {
+                speciality = value;
+            }
+        
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine(name + job + speciality + dutySchedule);
+        }
+        public Insert(int PWZ, string[] speciality, string name, string surname, int PESEL)
+        {
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Surname");
+            surname = Console.ReadLine();
+            Console.WriteLine("Choose speciality: [C]ardiologist, [U]rologist, [N]eurologist, [L]aryngologist");
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.C:
+                    speciality = speciality[0];
+                    break;
+                case ConsoleKey.U:
+                    speciality = speciality[1];
+                    break;
+                case ConsoleKey.N:
+                    speciality = speciality[2];
+                    break;
+                case ConsoleKey.L:
+                    speciality = speciality[3];
+                    break;
+
+
+
+            }
+            Console.WriteLine("Enter PESEL");
+            PESEL = Console.ReadLine();
+            Console.WriteLine("Enter PWZ number");
+            PWZ = Console.ReadLine();
+        */
+            
+
+
+
+        }
+        
+
+    }
+    class Cardiologist : Doctor
+    {
+    readonly String speciality = "Cardiologist";
+
+
+
+    }
+    class Urologist : Doctor
+    {
+    readonly string speciality = "Urologist";
+
+    }
+    class Neurologist : Doctor
+    {
+    readonly string speciality = "Neurologist";
+
+    }
+    class Laryngologist : Doctor
+    {
+    readonly string speciality = "Laryngologist";
+
+    }
+        
+    class Nurse : Scheduled
+    {
+        public Nurse(string name, string surname, int PESEL, string username, string password) : base(string name, string surname, int PESEL, string username, string password){ }
+        /*public override void Display()
+        {
+            base.Display();
+            Doctor docy = new Doctor();
+            Console.WriteLine(name + job + speciality + dutySchedule);
+        }*/
+        
+
+    }
+        
+            
+    
+}
